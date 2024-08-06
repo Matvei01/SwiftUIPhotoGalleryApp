a9fabf3bb73ac34f969eca9f8bb5e8c80ada30c5
Matvei <khlestov.matvei@mail.ru>
2024-08-06 20:43:32 +0500

    Added data model for app

---
+++ SwiftUIPhotoGalleryApp/SwiftUIPhotoGalleryApp/SwiftUIPhotoGalleryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPhotoGalleryAppApp.swift
+//  SwiftUIPhotoGalleryApp
+//
+//  Created by Matvei Khlestov on 06.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPhotoGalleryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
